d8cca012e819d0d1b341317cb3eee8f06333e13d
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2024-02-21 15:23:27 -0300

    feat: Update information for completed projects

--- curso-angular-portfolio/src/app/modules/portfolio/interface/IProjects.interface.ts
+++ curso-angular-portfolio/src/app/modules/portfolio/interface/IProjects.interface.ts
@@ -2,8 +2,8 @@ export interface IProjects {
     src: string;
     alt: string;
     title: string;
-    width: string;
-    height: string;
+    // width: string;
+    // height: string;
     description: string;
     links: [
       {
